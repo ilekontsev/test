1160b81106376910d217f31ea4535de33f16aea7
ilekontsev <84373567+ilekontsev@users.noreply.github.com>
2021-05-19 17:18:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
-
-
-let str = "Я изучаю JavaScript"
-
-let value = 0
-console.log(str[value])
-console.log(str.split("")[value])
-console.log(str.slice(value, value+1))
-for(let i =0; i<str.length; i++){
-    if(i == value){
-        console.log(str[i])
-        break
-    }
+let str = "one two"
+console.log(str.slice(1,5))
+​
+let arr = ["one", "two"]
+arr.splice(0, 1, "three")
+console.log(arr)
+​
+let array = [1,2,3,4]
+​
+​
+for(let i = 0; i<array.length; i++){
+    console.log(array[i])
 }
-
-lastSymbol =str.length-1
-console.log(str[lastSymbol])
-console.log(str.split("")[lastSymbol])
-console.log(str.slice(lastSymbol))
-console.log(str.slice(-1))
-value = lastSymbol
-for(let i =0; i<str.length; i++){
-    if(i == value){
-        console.log(str[i])
-        break
-    }
-}
+​
+​
+array.map(function(value){
+    console.log(`value+1: ${value+1}`)
+​
+} )
+​
+​
+array.forEach((value, index) => {
+    console.log(`index: ${index} value: ${value}`)
+});
+​
+​
+let mas = [
+    {name: "Djon"},
+    {name: "Mark"},
+    {name: "Tom"},
+]
+​
+​
+mas.filter(function(mas){
+    console.log(mas.name)
+})
+​
+​
+let a = array.reduce((sum, item)=>item + sum , 0 )
+console.log(a)
+​
+​
+let arr2 = [11, 1, 5, 22, 21, 3,5,7]
+​
+arr2.sort((a,b) => a - b)
+console.log(arr2)
+​
+​
+console.log(arr2.some((a) => a > 55 ))
+console.log(arr2.some((a) => a > 2 ))
+​
+​
+console.log(arr2.every((a) => a > 20))
+console.log(arr2.every((a) => a > 0))
